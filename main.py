cb586f4f0ee063c48ec87617f6e6855400c0c2da
HichemStinson <hamzareguighichem97@gmail.com>
2023-01-06 12:20:48 +0100

    update

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+from flask import Flask
+app = Flask(__name__) #definir le nom de l'application "app"
+@app.route('/') #initialiser un decorateur sur la route racine "/"
+
+def bonjour():
+    message = "Bonjour tout le monde \n"
+    return message
+
+if __name__ == '__main__': #appler les fonctions a l'interieur du script
+    app.run(host='0.0.0.0', port=8080)
