d5df5bd2724b1ffdb1fc803ca854d44827444c58
Lucas GAIO DOS SANTOS <lucas.gaildossantos@gmail.com>
2023-01-06 15:39:46 +0100

    add

--- mongo_helper.py
+++ mongo_helper.py
@@ -1,24 +1,30 @@
+from pymongo import MongoClient 
+
 class mongoHelper:
     def __init__(self, connect) -> None:
         # Constructeur
+        connect = "mongodb+srv://Bergfrid:1234@cluster0.em3v4rs.mongodb.net/?retryWrites=true&w=majority"
         # Enregistrer le client mongo
-        self.client = ""
+        self.client = MongoClient(connect)
         # Enregistrer la bdd voulu
         self.bdd = ""
         pass
 
     def collect(self, collection, donnees):
-        self.donnees = ""
+        
         pass
 
-    def add(self):
+    def add(self, collection, donnees):
         # Ajouter des données.
+        collection.insert_one({donnees})
         pass
 
-    def update(self):
+    def update(self, collection, donnees):
         # Moficier des données.
+        collection.update_one({donnees})
         pass
 
-    def remove(self):
+    def remove(self, collection, donnees):
         # Supprimer des données
+        collection.delete_one({donnees})
         pass
