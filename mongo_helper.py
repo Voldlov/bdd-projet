487977c940590aaf6699c07211d15fb8b64dc34c
Lucas GAIO DOS SANTOS <lucas.gaildossantos@gmail.com>
2023-01-06 15:03:25 +0100

    Code de base pour mongo_helper.py. Basic code for mongo_helper.py.

---
+++ mongo_helper.py
@@ -0,0 +1,12 @@
+class mongoHelper:
+    def __init__(self, connect) -> None:
+        # Constructeur
+        # Enregistrer le client mongo
+        self.client = ""
+        # Enregistrer la bdd voulu
+        self.bdd = ""
+        pass
+
+    def collect(self, collection, donnees):
+        self.donnees = ""
+        pass
