b23ad49c3afa0aaf12feb067ac5a71dc3cc66030
HichemStinson <hamzareguighichem97@gmail.com>
2023-01-12 15:31:54 +0100

    recuperation des features des tremblements de terre (un mois et la veille)

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python
-from flask import Flask
+import json
+from urllib import response
+
+from flask import Flask, request, redirect,url_for
+
+import mongo_helper
 
 app = Flask(__name__)  # definir le nom de l'application "app"
+
+def succes():
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
 
 @app.route('/')  # initialiser un decorateur sur la route racine "/"
@@ -9,6 +17,29 @@ def bonjour():
     message = "Bonjour tout le monde \n"
     return message
 
+@app.route('/data', methods=['POST'])
+def creat_data():
+    data = request.form["data"]
+    #mongo_helper.MongoHelper.add(data)
+    return succes()
+
+@app.route('/data', methods=['GET'])
+def result_data():
+    user = request.args.get('_id')
+    #mongo_helper.MongoHelper.get(user)
+    return succes()
+@app.route('/data', methods=['PUT'])
+def update_data():
+    data_id = request.args.get('_id')
+    data=request.form["data"]
+    #mongo_helper.MongoHelper.update(data)
+    return succes()
+
+@app.route('/data', methods=['DELETE'])
+def delete_data():
+    user = request.args.get('_id')
+    #mongo_helper.MongoHelper.remove(user)
+    return succes()
 
 if __name__ == '__main__':  # appler les fonctions a l'interieur du script
     app.run(host='0.0.0.0', port=8080)
