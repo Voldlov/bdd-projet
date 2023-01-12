119879de77adda21c7a820d8def28c9cb0baf145
HichemStinson <hamzareguighichem97@gmail.com>
2023-01-12 15:42:54 +0100

    recuperation des features des tremblements de terre (un mois et la veille)

--- main.py
+++ main.py
@@ -2,14 +2,20 @@
 import json
 from urllib import response
 
-from flask import Flask, request, redirect,url_for
+from flask import Flask, request, redirect, url_for
 
-import mongo_helper
+from earthquake import EarthquakeAPI
+from mongo_helper import MongoHelper
 
 app = Flask(__name__)  # definir le nom de l'application "app"
 
+
 def succes():
-    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+
+
+earthquakeAPI = EarthquakeAPI()
+mongo_helper = MongoHelper("mongodb://localhost:27017", "Tremblement")
 
 
 @app.route('/')  # initialiser un decorateur sur la route racine "/"
@@ -17,29 +23,36 @@ def bonjour():
     message = "Bonjour tout le monde \n"
     return message
 
+
 @app.route('/data', methods=['POST'])
-def creat_data():
+def create_data():
+
     data = request.form["data"]
-    #mongo_helper.MongoHelper.add(data)
+    mongo_helper.add(data)
     return succes()
+
 
 @app.route('/data', methods=['GET'])
 def result_data():
     user = request.args.get('_id')
-    #mongo_helper.MongoHelper.get(user)
+    # mongo_helper.get(user)
     return succes()
+
+
 @app.route('/data', methods=['PUT'])
 def update_data():
     data_id = request.args.get('_id')
-    data=request.form["data"]
-    #mongo_helper.MongoHelper.update(data)
+    data = request.form["data"]
+    # mongo_helper.update(data)
     return succes()
+
 
 @app.route('/data', methods=['DELETE'])
 def delete_data():
     user = request.args.get('_id')
-    #mongo_helper.MongoHelper.remove(user)
+    # mongo_helper.remove(user)
     return succes()
+
 
 if __name__ == '__main__':  # appler les fonctions a l'interieur du script
     app.run(host='0.0.0.0', port=8080)
