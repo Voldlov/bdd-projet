5892f9f9879caa58d8d825877eb0b1ea77fcc832
Axel Pélerin <arcanynka@gmail.com>
2023-01-26 14:49:20 +0100

    Add aggregation pipeline to link earthquake and weather data

--- mongo_helper.py
+++ mongo_helper.py
@@ -1,5 +1,11 @@
+from datetime import datetime, timedelta
+
 from bson import ObjectId
 from pymongo import MongoClient
+
+weather_collection = "weather"
+earthquake_collection = "earthquake"
+aggregate_collection = "aggregate"
 
 
 class MongoHelper:
@@ -18,8 +24,8 @@ class MongoHelper:
         self.bdd[collection].drop()
 
     def index_earthquake_id_on_earthquake_and_weather(self):
-        self.bdd["weather"].create_index("earthquake_id")
-        self.bdd["earthquake"].create_index("id")
+        self.bdd[weather_collection].create_index("earthquake_id")
+        self.bdd[earthquake_collection].create_index("id")
 
     """
         CRUD
@@ -29,9 +35,9 @@ class MongoHelper:
         # Ajouter des données.
         self.bdd[collection].insert_many(data)
 
-    def update(self, collection, data):
+    def update(self, collection, ids, data):
         # Modifier des données.
-        self.bdd[collection].update_many(data)
+        self.bdd[collection].update_many(ids, data)
 
     def remove(self, collection, data):
         # Supprimer des données
@@ -63,15 +69,94 @@ class MongoHelper:
         # Affection l'agregate à une collection et le retourner
         return collection.aggregate(pipeline)
 
-    def agregate_weather_and_earthquake(self, collection):
-        # Aggregate weather data corresponding to one earthquake
-        pipeline = [
-            {
-                "$lookup": {
-                    "from": "weather",
-                    "localField": "id",
-                    "foreignField": "earthquake_id",
-                    "as": "weather"
+    def agregate_weather_and_earthquake(self):
+        # agregate weather data the hour before and after an earthquake to the earthquake
+
+        # filter earthquakes which time is not between 22:59:59 and 1:00:00
+        project_timestamp_to_hour_in_earthquake = {
+            "$project": {
+                "id": 1,
+                "_id": 0,
+                "properties": 1,
+                "geometry": 1,
+                "yesterday": 1,
+                "hour": {
+                    # Here is the operator $hour to get the hour of the timestamp
+                    "$hour": {
+                        "$toDate": "$properties.time"
+                    }
                 }
-            }]
-        return self.bdd[collection].aggregate(pipeline)
+            }
+        }
+
+        filter_earthquake_not_between_22_59_59_and_1_00_00 = {
+            "$match": {
+                "$and": [
+                    {
+                        "hour": {
+                            "$lte": 22
+                        }
+                    },
+                    {
+                        "hour": {
+                            "$gte": 1
+                        }
+                    }
+                ]
+            }
+        }
+
+        lookup = {
+            "$lookup": {
+                "from": weather_collection,
+                "localField": "id",
+                "foreignField": "earthquake_id",
+                "as": "weather"
+            }
+        }
+        unwind = {
+            "$unwind": "$weather"
+        }
+
+        project_array_weather_forecast_forecastday_to_object = {
+            "$project": {
+                "id": 1,
+                "properties": 1,
+                "geometry": 1,
+                "yesterday": 1,
+                "hour": 1,
+                "weather": {"$arrayElemAt": ["$weather.forecast.forecastday", 0]}
+            }
+        }
+
+        project_weather_data_of_the_hour_before_and_after_the_earthquake = {
+            "$project": {
+                "id": 1,
+                "properties": 1,
+                "geometry": 1,
+                "yesterday": 1,
+                "weather_before": {"$arrayElemAt": ["$weather.hour", "$hour"]},
+                "weather_after": {"$arrayElemAt": ["$weather.hour", {"$add": ["$hour", 1]}]},
+            }
+        }
+
+        addObjectId = {
+            "$addFields": {
+                "_id": {
+                    "$toString": "$id"
+                }
+            }
+        }
+
+        pipeline = [
+            project_timestamp_to_hour_in_earthquake,
+            filter_earthquake_not_between_22_59_59_and_1_00_00,
+            lookup,
+            unwind,
+            project_array_weather_forecast_forecastday_to_object,
+            project_weather_data_of_the_hour_before_and_after_the_earthquake,
+            addObjectId
+        ]
+
+        agregate = self.bdd[earthquake_collection].aggregate(pipeline)
+        self.bdd[aggregate_collection].insert_many(agregate)
--- weather.py
+++ weather.py
@@ -10,10 +10,9 @@ class WeatherAPI:
     API_KEY = os.getenv('API_KEY')
     API_URL = 'http://api.weatherapi.com/v1/history.json?'
 
-    @staticmethod
-    def weather_information(lat: int, long: int, date: str):
+    def weather_information(self, lat: int, long: int, date: str):
         payload = {'q': f'{lat},{long}', 'dt': f'{date}'}
-        headers = {'key': f'{WeatherAPI.API_KEY}'}
-        location = r.get(WeatherAPI.API_URL, params=payload, headers=headers)
+        headers = {'key': f'{self.API_KEY}'}
+        location = r.get(self.API_URL, params=payload, headers=headers)
         output = json.loads(location.text)
         return output
